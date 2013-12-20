3fe30bdd1320cd0099523d4216a2b6ca583d9b98
John-Philip Johansson <seriema@gmail.com>
2013-12-20 14:56:13 +0100

    Added settings flyout

--- src/Testudo/MainPage.xaml.cs
+++ src/Testudo/MainPage.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+using Windows.UI.ApplicationSettings;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +13,60 @@ namespace Testudo
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public static MainPage Current;
+
         public MainPage()
         {
             this.InitializeComponent();
+            Current = this;
+
+            Browser.Navigate(new Uri("http://xkiab7.axshare.com/home.html"));
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            SettingsPane.GetForCurrentView().CommandsRequested += onCommandsRequested;
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+
+            SettingsPane.GetForCurrentView().CommandsRequested -= onCommandsRequested;
+        }
+
+        /// <summary>
+        /// Handler for the CommandsRequested event. Add custom SettingsCommands here.
+        /// </summary>
+        /// <param name="e">Event data that includes a vector of commands (ApplicationCommands)</param>
+        void onCommandsRequested(SettingsPane settingsPane, SettingsPaneCommandsRequestedEventArgs e)
+        {
+            SettingsCommand defaultsCommand = new SettingsCommand("general", "General",
+                (handler) =>
+                {
+                    // Settings is defined in "Settings.xaml"
+                    Settings sf = new Settings();
+                    sf.Show();
+                });
+            e.Request.ApplicationCommands.Add(defaultsCommand);
+        }
+
+
+        public void SetUrl(Uri url)
+        {
+            try
+            {
+                Browser.Navigate(url);
+            }
+            catch (Exception)
+            {
+                // Hack: Being a bad boy!
+            }
         }
     }
 }
---
+++ src/Testudo/Settings.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+
+// The Settings Flyout item template is documented at http://go.microsoft.com/fwlink/?LinkId=273769
+
+namespace Testudo
+{
+    public sealed partial class Settings : SettingsFlyout
+    {
+        public Settings()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BrowserButton_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            try
+            {
+                MainPage.Current.SetUrl(new Uri(BrowserUrl.Text));
+                BrowerError.Text = "";
+            }
+            catch (FormatException)
+            {
+                BrowerError.Text = "Not a valid URL.";
+            }
+        }
+
+    }
+}
